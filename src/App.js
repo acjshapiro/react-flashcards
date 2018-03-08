a6142c67e0fd492cf4a76bc9676ccb5ce9e7e997
Alex Shapiro <acjshapiro@gmail.com>
2018-03-07 18:41:51 -0700

    cards

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { Container, Divider, Header } from 'semantic-ui-react';
 
 
 class App extends Component {
-  state = { cards: [ {id: 1, front: 'Arrow Functions.', back: 'Are Super Dumb.'},
+  state = { cards: [ {id: 1, front: 'Arrow Functions.', back: 'Are hard for me to read... But once I learn the syntax will probably be awesome'},
   {id: 2, front: 'Ezekiel 25:17', back: "The path of the righteous man is beset on all sides by the inequities of the selfish and the tyranny of evil men. Blessed is he, who in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy my brothers. And you will know my name is the Lord when I lay my vengeance upon thee."},
-  {id: 3, front: 'Arrow Functions.', back: 'Are Super Dumb.'},
+  {id: 3, front: 'es6', back: 'is pretty cool. But also confusing as I was just starting to learn Vanilla JS'},
 
 ]}
 
